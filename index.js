86f69f73415287682d9cd5e12b0db0ff8b8b8549
Samuel Ogu <hello@samuelogu.com>
2023-01-19 02:25:54 +0100

    done

---
+++ index.js
@@ -0,0 +1,6 @@
+const Web3 = require('web3');
+const rpcURL = "https://bsc-dataseed1.binance.org:443";
+const web3 = new Web3(rpcURL);
+
+const account = web3.eth.accounts.create();
+console.log(account);
